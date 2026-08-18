3fb95eae9c662436ca1e8e2b6707e423495e1e0f
priyanshu5179 <priyanshuyadav876585@gmail.com>
2026-08-18 14:24:33 +0530

    Add Conditional Statement program

---
+++ src/ConditionalStatement.java
@@ -0,0 +1,46 @@
+public class ConditionalStatement {
+    static void main() {
+//        int a=56;
+//                if (a>=43){
+//                    System.out.println("a value is less than condition value");
+//        int age =18;
+//        if (age>=18){
+//            System.out.println(" the person is eligible for vote ");
+//                }
+//        else {
+//            System.out.println("the person is not eligable for vote");
+
+//                char person = 'M';
+//                int age = 23;
+//
+//                if (person == 'M') {
+//
+//                    if (age >= 18) {
+//                        System.out.println("The person is male and is eligible for vote.");
+//                    } else {
+//                        System.out.println("The person is male but not eligible for vote.");
+//                    }
+//
+//                } else {
+//
+//                    if (age >= 18) {
+//                        System.out.println("The person is female and is eligible for vote.");
+//                    } else {
+//                        System.out.println("The person is female and is not eligible for vote.");
+//                    }
+//
+//                }
+//        int solvedProblems = 320;
+//
+//        String level = (solvedProblems >= 300) ? "Advanced" : "Intermediate";
+//
+//        System.out.println(level);
+//            }
+//        }
+
+        int age = 10;
+        StringBasic agelimit = (age <= 18) ? "not eligble" : "eligble";
+        System.out.println("age limit:"+ agelimit);
+    }
+
+}
