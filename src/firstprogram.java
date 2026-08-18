db2611f280db8f8ded914a36af1a8f9f7c9e7b5f
priyanshu5179 <priyanshuyadav876585@gmail.com>
2026-08-18 11:25:27 +0530

    first programming in java

---
+++ src/firstprogram.java
@@ -0,0 +1,21 @@
+void main() {
+    System.out.println("hello world" + "hello world");
+    short num1 = 50;
+    System.out.println(num1);
+    int num2 = 3444;
+    System.out.println(num2);
+    long num3 = 23521713;
+    float num4 = 6.8878f;
+    System.out.println(num4);
+
+    double num5 = 6.9586723459f;
+    System.out.println(num5);
+
+
+   /* char first_character='a';
+    System.out.println(first_character);
+    */
+    char first_character = 'a';
+    System.out.println("my first character:" +(char) (first_character + 2));
+
+}
