4f11e42b52ff1da00953a48e176708465de4ffe1
priyanshu5179 <priyanshuyadav876585@gmail.com>
2026-08-20 12:35:13 +0530

    Useing loop method

---
+++ src/Loop.java
@@ -0,0 +1,20 @@
+//public class Loop {
+//    static void main() {
+//        for (int i = 2; i <= 10; i++) {
+//            System.out.println("i love you ");
+//     NEXT CODE
+//      for( int i =1; i<=3; i++) {
+//          for (int j=2; j<=4;j++){
+//              System.out.print("* ");
+//          }
+//         System.out.println("");
+////        NEXT CODE
+//        for( int i =1; i<=3; i++) {
+//            for (int j=2; j<=4;j++){
+//                System.out.print( i)}
+//            System.out.println("");
+//
+//
+//        }
+//    }
+//}
