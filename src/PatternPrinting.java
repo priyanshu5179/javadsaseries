07fc046c0ca6f1a1fc370fc997b7484f30eff790
priyanshu5179 <priyanshuyadav876585@gmail.com>
2026-08-23 22:49:40 +0530

    Useing  all method

--- src/PatternPrinting.java
+++ src/PatternPrinting.java
@@ -57,18 +57,18 @@
 //                }
 //            }
 //       NEXT CODE
-                 int n = 4;
-
-                 for (int row = 1; row <= n; row++) {
-                     for (int col = 1; col <= 6; col++) {
-
-                         if (row == 1 || row == n || col == 1 || col == 6) {
-                             System.out.print("* ");
-                         } else {
-                             System.out.print("  ");
-                         }
-                     }
-                     System.out.println();
-                 }
-             }
-         }
+//                 int n = 4;
+//
+//                 for (int row = 1; row <= n; row++) {
+//                     for (int col = 1; col <= 6; col++) {
+//
+//                         if (row == 1 || row == n || col == 1 || col == 6) {
+//                             System.out.print("* ");
+//                         } else {
+//                             System.out.print("  ");
+//                         }
+//                     }
+//                     System.out.println();
+//                 }
+//             }
+//         }
