6eb6a7113a244bd9861a151418fd76917142496c
priyanshu5179 <priyanshuyadav876585@gmail.com>
2026-09-04 21:39:52 +0530

    practice oops

--- src/OOPS/PrivateKeyword.java
+++ src/OOPS/PrivateKeyword.java
@@ -1,12 +1,20 @@
 package OOPS;
-
+class student{
+    String name;
+    private int rno;
+    double cgpa;
+}
 public class PrivateKeyword {
-    public static class student{
-        String name;
-        int rno;
-        double cgpa;
-    }
+//    public static class student{
+//        String name;
+//        private int rno;
+//        double cgpa;
+//    }
     static void main() {
-
+        student s1 =new student();
+        System.out.println(s1.cgpa);
+        s1.cgpa=34.5;
+        s1.name="priyanshu";
+//        s1.rno =45;
     }
 }
